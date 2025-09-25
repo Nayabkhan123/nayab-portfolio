a42ba06b35b295fbf0408bb19a5b8c43d366d71d
Nayab Khan <160863567+Nayabkhan123@users.noreply.github.com>
2025-09-26 00:50:14 +0530

    updated resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1o8uMT-d8aGx6Ap0FUaLqWIZljmBNzNtv/view"
+            href="https://drive.google.com/file/d/11XYsoPzbQnUNgoiW2Oy16IHh07xE3Cpu/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
